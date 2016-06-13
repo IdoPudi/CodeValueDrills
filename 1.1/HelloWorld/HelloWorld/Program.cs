ab5980e89bb83505445af890766c2307be49bf11
Ido Podhajcer <Ido Podhajcer@IDOPC>
2016-06-13 13:03:51 +0300

    1.1 and 1.2 drills

---
+++ 1.1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, C#!");
+            Console.WriteLine("Ido Podhajcer");
+            Console.WriteLine("bnei Binyamin 13 apt 47, Netanya, zip 421966");
+        }
+    }
+}

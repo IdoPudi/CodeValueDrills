d0c719a67c5b0d7c01709d8b509fc46cd9afa9cf
Ido Podhajcer <Ido Podhajcer@IDOPC>
2016-07-22 22:52:13 +0300

    Advanced Lab 2

---
+++ Advanced/Lab 2/2.2/GenericApp/GenericApp/MultiDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApp
+{
+    
+    public class MultiDictionary<K, V> : IMultiDictionary<K, V>, IEnumerable<KeyValuePair<K, IEnumerable<V>>> 
+        where K : struct
+        where V : new()
+    {
+        Dictionary<K, LinkedList<V>> dictionary = new Dictionary<K, LinkedList<V>>();
+
+        public int Count => dictionary.SelectMany(d => d.Value).Count();
+        public ICollection<K> Keys => dictionary.Keys;
+        public ICollection<V> Values => dictionary.SelectMany(d => d.Value).ToList();
+
+        
+        public void Add(K key, V value)
+        {
+            bool hasAttribute = false;
+            var m = key.GetType();
+
+            if (m.GetCustomAttributes(typeof(KeyAttribute),true).Length > 0)
+                hasAttribute = true;
+
+            if (!hasAttribute)
+                throw new Exception("Key must have attribute");
+
+            if (!dictionary.ContainsKey(key))
+                dictionary.Add(key, new LinkedList<V>());
+
+            dictionary[key].AddLast(value);
+
+        }
+
+        public void Clear() => dictionary.Clear();
+        public bool Contains(K key, V value) => dictionary.ContainsKey(key) && dictionary[key].Contains(value);
+        public bool ContainsKey(K key) => dictionary.ContainsKey(key);
+
+        public void CreateNewValue(K key) => Add(key, new V());
+        
+
+        public IEnumerator<KeyValuePair<K, IEnumerable<V>>> GetEnumerator() =>
+            dictionary.Select(p => new KeyValuePair<K, IEnumerable<V>>(p.Key, p.Value)).GetEnumerator();
+        public bool Remove(K key) => dictionary.Remove(key);
+        public bool Remove(K key, V value) => dictionary[key].Remove(value);
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Advanced/Lab 2/2.2/GenericApp/GenericApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiDictionary<FirstToDictionary, StringBuilder> dictionary = new MultiDictionary<FirstToDictionary, StringBuilder>();
+
+
+            FirstToDictionary first;
+            first.number = 1;
+
+            dictionary.CreateNewValue(first);
+            foreach (var item in dictionary.Values)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine(dictionary.Count());
+        }
+    }
+
+    [Key]
+    public struct FirstToDictionary
+    {
+        public int number;
+    }
+}
